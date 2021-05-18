2532f2c86681ed0bae7e5d703148371042a39e3b
Manish Rawal <rawalsinghmanish@gmail.com>
2021-05-18 17:04:18 +0545

    suggesting-for-non-existing-word-implemented

--- app1.py
+++ app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,8 @@ data = json.load(open("data.json"))
 def translate(w):
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:
         return "The word doesn't exist,please check it"
 
