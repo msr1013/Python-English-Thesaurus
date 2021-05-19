2662fcd4b485599af4c201b1199be97ca9a7d9d4
Manish Rawal <rawalsinghmanish@gmail.com>
2021-05-19 18:03:05 +0545

    confirming-from-user-optimizing-code-to-display-list

--- app1.py
+++ app1.py
@@ -11,13 +11,24 @@ def translate(w):
         inp = input("Did you mean %s instead? Enter y if Yes or n if No :" %
                     get_close_matches(w, data.keys())[0])
         if inp == 'y':
-            print(data[get_close_matches(w, data.keys())[0]])
+            return data[get_close_matches(w, data.keys())[0]]
+        elif inp == 'n':
+            return 'The word doesnt exist,please double check it!'
 
         else:
-            return "The word doesn't exist,please check it"
+            return "We donot understand your entry"
+
+    else:
+        return "Sorry no matches,word doesnt exist please double check"
 
 
 result = input("enter word: ")
 result = result.lower()
 
-print(translate(result))
+output = translate(result)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
